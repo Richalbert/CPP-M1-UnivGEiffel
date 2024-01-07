b33200b3e2d7ea8b47911774d556f9065065d7e2
Richalbert <richalbert@free.fr>
2024-01-07 19:03:18 +0100

    Ma nouvelle version d'Hello world

--- chap-01/1-hello_world.cpp
+++ chap-01/1-hello_world.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 int main() {
 
     // Ecrire sur la sortie standard
     std::cout << "Hello world!" << std::endl;
+
+
+    // Autre version
+    std::vector<std::string> msg = {"Hello", "world", "!"};
+
+    for (const std::string& word : msg) {
+        std::cout << word << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
