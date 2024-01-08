e68b116469330f13fed1677f1d414f81d631e2dc
Richalbert <richalbert@free.fr>
2024-01-08 23:46:44 +0100

    une structure

---
+++ chap-01/ex6-type-structure.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+
+struct Fraction {
+    public:
+        void setNum(int num) {
+            _num = num;
+        }
+
+        void setDen(int den) {
+            _den = den;
+        }
+
+        void add(Fraction other) {
+            if (_den == other._den) {
+                _num += other._num;
+            } else {
+                _den *= other._den;
+                _num = _num * other._den + other._num * _den;
+            }
+        }
+
+        void print() {
+            std::cout << _num << '/' << _den << std::endl;
+        }
+
+    private:
+        int _num = 0;
+        int _den = 1;
+};
+
+int main() {
+
+    Fraction f1;
+    f1.setNum(3);
+    f1.setDen(6);
+    f1.print();
+
+    Fraction f2;
+    f2.setNum(-1);
+    f2.setDen(6);
+    f2.print();
+
+    f1.add(f2);
+    f1.print();
+
+    Fraction f3;
+    f3.setNum(2);
+    f3.print();
+
+    f1.add(f3);
+    f1.print();
+
+    return 0;
+}
