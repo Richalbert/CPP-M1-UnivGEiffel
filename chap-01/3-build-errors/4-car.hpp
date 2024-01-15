8d3ab74558db35814e9641e50b5c0aa330962634
Richalbert <richalbert@free.fr>
2024-01-15 18:43:51 +0100

    directive #pragma once au sommet de chaque header

--- chap-01/3-build-errors/4-car.hpp
+++ chap-01/3-build-errors/4-car.hpp
@@ -1,3 +1,7 @@
+#pragma once    // directive au sommet de chaque headerde chaque header
+                // pour empecher les inclusions multiples d'un meme header
+                // plus moderne que #ifndef, #define, #endif
+
 #include <string>
 
 struct Car
