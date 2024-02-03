ebb646cdae11d5cc313bed11da774b5ddf4e3d22
Richalbert <richalbert@free.fr>
2024-02-03 19:59:45 +0100

    ctor Person a la place du setter name

--- chap-02/2-constructor.cpp
+++ chap-02/2-constructor.cpp
@@ -7,8 +7,12 @@ public:
     const std::string& get_name() const { return _name; }
     unsigned int       get_age() const { return _age; }
 
-    void set_name(const std::string& name) { _name = name; }
+    //void set_name(const std::string& name) { _name = name; }
     void set_age(unsigned int age) { _age = age; }
+
+    /* ctor */
+    Person(const std::string& name) : _name { name } {}
+   
 
 private:
     std::string  _name;
@@ -17,12 +21,55 @@ private:
 
 int main()
 {
-    Person p;
+    Person p {"Batman"};
 
-    p.set_name("Batman");
+    //p.set_name("Batman");
     p.set_age(23);
 
     std::cout << "Person named '" << p.get_name() << "' is " << p.get_age() << " years old." << std::endl;
 
     return 0;
 }
+
+/*
+    Methodologie Test Driven Development - TDD
+    - on part du main, et
+    - on modifie le code pour que ca compile
+
+    Compilation seule
+        g++ -std=c++17 -Wfatal-errors -c 2-constructor.cpp
+
+    Compilation avec les symboles de debug
+        g++ -g -std=c++17 -o 2-constructor 2-constructor.cpp
+
+        puis configurer le fichier.vscode/launch.json
+        ...
+        "program": "${workspaceFolder}/chap-02/2-constructor",
+        ...
+
+    Compilation et edition de liens
+        g++ -std=c++17 -Wfatal-errors -o program 2-constructor.cpp
+
+    Execution
+        ./program
+
+    Sortie
+        Person named 'Batman' is 23 years old.
+
+    Git
+        git branch -D test              // delete de la branche test en local
+        git branch test                 // creation de la branche en local
+        git checkout test               // changement de branche
+        git status                      // pour voir
+        git add 2-constructor.cpp       
+        git commit -m "TDD - ca compile"
+        git push origin test-ch2
+
+        git checkout master
+        git merge test-ch2
+        git commit -m "merge de la branche de test-ch2"
+        git push origin master
+
+        git branch -d test-ch2          // suppression de la branche en local
+        git push origin -d test-ch2     // suppression de la branche sur le depot distant
+*/
