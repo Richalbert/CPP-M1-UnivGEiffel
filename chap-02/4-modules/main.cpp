6eeb4bd9af116ff417b2b8b5d4ec1cc13365521b
Richalbert <richalbert@free.fr>
2024-02-09 16:51:05 +0100

    commentaire sur le inline automatique

--- chap-02/4-modules/main.cpp
+++ chap-02/4-modules/main.cpp
@@ -12,3 +12,26 @@ int main()
 
     return 0;
 }
+
+/*
+    g++ -std=c++17 -Wfatal-errors -o program main.cpp Rectangle.cpp
+
+    compilation seule
+    g++ -std=c++17 main.cpp Rectangle.cpp
+
+
+    main.o: 
+        - main()
+        - Rectangle::get_length()       // apparait 2 fois
+        - Rectangle::get_width()        // donc ne devrait pas compiler
+
+    Rectangle.o:
+        - Rectangle::get_length()       <-- definie inline
+        - Rectangle::get_width()        <-- definie inline
+        - Rectangle::Rectangle()        <-- definie inline
+        - Rectangle::sclae(float)       <-- definie inline
+
+    Toutes les fonctions definies a l'interieur de la definition 
+    de la classe sont automatiquement inline
+
+*/
