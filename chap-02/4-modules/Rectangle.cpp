b214bdc75d057bd23da1abe1af94abf4f844e5ee
Richalbert <richalbert@free.fr>
2024-02-09 16:34:05 +0100

    definition du constructeur et de la fonction

--- chap-02/4-modules/Rectangle.cpp
+++ chap-02/4-modules/Rectangle.cpp
@@ -1,1 +1,15 @@
 // Implement Rectangle functions here.
+
+#include <Rectangle.h>
+
+Rectangle::Rectangle(float length, float width)
+        : _length { length }
+        , _width { width }
+    {}
+
+
+void Rectangle::scale(float ratio)
+    {
+        _length *= ratio;
+        _width *= ratio;
+    }
