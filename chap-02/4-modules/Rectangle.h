cd57f8f6e7080d0f2a2c5f4d07467b4e164e05e3
Richalbert <richalbert@free.fr>
2024-02-09 16:32:58 +0100

    signature du constructeur et de la fonction

--- chap-02/4-modules/Rectangle.h
+++ chap-02/4-modules/Rectangle.h
@@ -1,21 +1,31 @@
 #pragma once
 
+/* TODO
+*   extraire le constructeur de la classe Rectangle
+*   et la fonction scale dans le fichier Rectangle.cpp
+*/
+
 class Rectangle
 {
 public:
-    Rectangle(float length, float width)
-        : _length { length }
-        , _width { width }
-    {}
+    
+    Rectangle(float length, float width);
+    // Rectangle(float length, float width)
+    //     : _length { length }
+    //     , _width { width }
+    // {}
 
+    /* getter */
     float get_length() const { return _length; }
     float get_width() const { return _width; }
 
-    void scale(float ratio)
-    {
-        _length *= ratio;
-        _width *= ratio;
-    }
+
+    void scale(float ratio);
+    // void scale(float ratio)
+    // {
+    //     _length *= ratio;
+    //     _width *= ratio;
+    // }
 
 private:
     float _length;
